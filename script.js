14657ca47f5e34806615ae94fe48dca6c24687c6
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-09-28 09:58:53 +0200

    Implemented localstorage for visitorcount function

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function countVisitors () {
     const visitors = document.getElementById('visitors');
-    let count = 0;
+    let count = 0 + localStorage.getItem('count');
     count ++;
+    localStorage.setItem('count', count)
     visitors.innerHTML += count    
 }
 
