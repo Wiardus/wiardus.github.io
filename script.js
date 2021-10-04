063d18347de5915bf8cd1ce19bb640043c8ed1bc
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-10-04 22:59:11 +0200

    Added experience

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 function countVisitors () {
     const visitors = document.getElementById('visitors');
     let count = 0 + localStorage.getItem('count');
@@ -6,3 +8,14 @@ function countVisitors () {
     visitors.innerHTML += count    
 }
 
+let split = new SplitText(".name")
+
+let tl = gsap.timeline({scrollTrigger:{
+  trigger:".name",
+  start:"top 50%",
+  end:"bottom top",
+  toggleActions:"restart none none reset"
+}})
+.from(split.chars, {yPercent:-100,  stagger:0.05, duration:0.3, ease:"back"})
+.from(split.chars, {opacity:0, stagger:0.05, duration:0.2}, 0)
+.from("button", {y:100, opacity:0, ease:"back", duration:1}, "<1")
